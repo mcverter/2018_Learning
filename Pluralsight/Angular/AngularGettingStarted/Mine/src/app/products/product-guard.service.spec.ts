2f12223b9600d92fae1d67e6a1aedbf16a905d57
mcverter <mitchell.verter@gmail.com>
2018-01-30 00:14:51 -0500

    Angular overview

---
+++ Pluralsight/Angular/AngularGettingStarted/Mine/src/app/products/product-guard.service.spec.ts
@@ -0,0 +1,14 @@
+import {inject, TestBed} from '@angular/core/testing';
+import {ProductGuardService} from './product-guard.service';
+
+describe('ProductGuardService', ()=>{
+  beforeEach(()=>{
+    TestBed.configureTestingModule({
+      providers: [ProductGuardService]
+    })
+  })
+
+  it ('should be created', inject([ProductGuardService], (service: ProductGuardService)=>{
+    expect(service).toBeTruthy();
+  }));
+});
